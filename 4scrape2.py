589d6fcf5fbf607232753cb24c061ab699da9c6b
Meininger <reimage@reimage-macbook.ant.amazon.com>
2017-08-22 15:34:58 -0700

    4scrape

---
+++ 4scrape2.py
@@ -0,0 +1,45 @@
+
+#python3
+import sys, bs4, requests, os
+
+
+def debug(dbug):
+	print(dbug)
+
+if len(sys.argv) > 1:
+	specifiedBoard = sys.argv[1]
+else:
+		print("Specify board letter")
+		specifiedBoard = input()
+
+urlFull = "http://4chan.org/" + specifiedBoard
+
+debug("The urlFull variable value is " + urlFull)
+
+markup = requests.get(urlFull)
+markup.raise_for_status()
+
+searchableMarkup = bs4.BeautifulSoup(markup.text,"lxml")
+#markupText = markup.text
+
+#print(markup.url)
+
+# 1. Pull all image tags from HTML (DONE)
+allImages = searchableMarkup.select('img')
+print(allImages)
+
+# 2. Filter out ads/banners
+
+#3. Check for existing scrape directory and create one if necessary to store files
+folder = os.path.expanduser('~/Pictures')
+#This line returns only immediate child folders of the 'Pictures' folder
+dir_list = next(os.walk(folder))[1]
+if '4scrape' not in str(dir_list):
+	try:
+		#creates a folder called '4scrape' in the Pictures directory
+		os.mkdir(folder + '/4scrape')
+		print("Creating directory")
+	except:
+		print("Directory already exists")
+print("This is a list of the directories under 'Pictures' " + str(dir_list))
+#4. Write images to disk
